be7103c764c894ee672dbd586a89815d10a57248
vbstreetz <vbstreetz@gmail.com>
2020-08-01 18:07:05 +0300

    fix cron schedule

--- backend/bin/cron.js
+++ backend/bin/cron.js
@@ -1,16 +1,17 @@
-const cron = require('node-cron');
-const {Web3, Contract} = require('./w');
-const CONTRACT_JSON = require('../build/contracts/Bet.json');
+const cron = require("node-cron")
+const {Web3, Contract} = require("./w")
+const CONTRACT_JSON = require("../build/contracts/Bet.json")
 
-cron.schedule("* 1 * * *", async function() {
-  console.log("running at", new Date());
+cron.schedule("5,55 * * * *", async function () {
+  // hourly: 0005, 0055, 0105, 0155 ...
+  console.log("running at", new Date())
 
   try {
-    const web3 = Web3();
-    const [account] = await web3.eth.getAccounts();
+    const web3 = Web3()
+    const [account] = await web3.eth.getAccounts()
     const contract = new Contract(web3, 4, CONTRACT_JSON, account)
-    console.log(await contract.write('saveCurrentDayRankingFromChainlink'));
-  } catch(e) {
-    console.log(e);
+    console.log(await contract.write("saveCurrentDayRankingFromChainlink"))
+  } catch (e) {
+    console.log(e)
   }
-});
+})
